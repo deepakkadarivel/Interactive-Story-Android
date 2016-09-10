55cbb9bf180e562ddb7cc1d56d5365264746c803
deepak <deepak.kadarivel@gmail.com>
2016-09-10 22:05:58 +0530

    Landing screen with Image, Edittext field and Button

--- app/src/main/java/com/upbeat/interactivestory/MainActivity.java
+++ app/src/main/java/com/upbeat/interactivestory/MainActivity.java
@@ -2,12 +2,32 @@ package com.upbeat.interactivestory;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = MainActivity.class.getSimpleName();
+    private EditText mNameEditText;
+    private Button mStartButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mNameEditText = (EditText) findViewById(R.id.nameEditText);
+        mStartButton = (Button) findViewById(R.id.startButton);
+
+        mStartButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String name = mNameEditText.getText().toString();
+
+                Toast.makeText(MainActivity.this, name, Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
